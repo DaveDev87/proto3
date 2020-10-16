f8f62890f879b0b4c9d589ef4c58ed43604094f6
Dave <alexruedarivas@outlook.com>
2020-10-16 14:47:18 -0500

    gridsome

---
+++ src/layouts/Default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nav class="navbar navbar-dark bg-dark">
+      <span class="navbar-brand mb-0 h1">Proto 3</span>
+    </nav>
+    <slot />
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style scope></style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Layout>
+
+  <h1>Main feed</h1>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Home!'
+  }
+}
+</script>
